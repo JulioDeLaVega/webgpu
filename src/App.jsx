e41c9f2e20ce938c577754a806ab6de67c006f72
JulioDeLaVega <jules.chancel@laposte.net>
2025-04-04 16:05:50 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,389 @@
+import { useEffect, useState, useRef } from "react";
+
+import { DownloadCloud, PauseCircle } from 'react-feather';
+
+
+import Chat from "./components/Chat";
+import ArrowRightIcon from "./components/icons/ArrowRightIcon";
+import Progress from "./components/Progress";
+
+const IS_WEBGPU_AVAILABLE = !!navigator.gpu;
+const STICKY_SCROLL_THRESHOLD = 120;
+const EXAMPLES = [
+  "Solve the equation x^2 - 3x + 2 = 0",
+  "Lily is three times older than her son. In 15 years, she will be twice as old as him. How old is she now?",
+  "Write python code to compute the nth fibonacci number.",
+];
+
+function App() {
+  // Create a reference to the worker object.
+  const worker = useRef(null);
+
+  const textareaRef = useRef(null);
+  const chatContainerRef = useRef(null);
+
+  // Model loading and progress
+  const [status, setStatus] = useState(null);
+  const [error, setError] = useState(null);
+  const [loadingMessage, setLoadingMessage] = useState("");
+  const [progressItems, setProgressItems] = useState([]);
+  const [isRunning, setIsRunning] = useState(false);
+
+  // Inputs and outputs
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [tps, setTps] = useState(null);
+  const [numTokens, setNumTokens] = useState(null);
+
+  function onEnter(message) {
+    if (!message || message.trim() === "" || message === "") return;
+    const prompt =  
+    "You are a senior legal assistant in a large company. You need to help your colleagues on what to do only based on the context. Don't think too much, only use the context to answer. /n Context: {Always provide concise and direct answers without over-explaining. For answering, you need to take ONLY and ONLY these company rules into account. (rule 1) Compliance: Each new supplier and client must be screened by compliance. For this contact compliance@ingroupe.com. (rule 2) Legal: Legal must always be informed of new tenders, contractual documentation of the tender must be sent to legal@ingroupe.com with the deal value and name of client.} /n Question: {" + message + "}"
+    console.log(messages)
+    setMessages((prev) => [...prev, { role: "user", content: message}]);
+    setTps(null);
+    setIsRunning(true);
+    setInput("");
+  }
+
+  function onInterrupt() {
+    // NOTE: We do not set isRunning to false here because the worker
+    // will send a 'complete' message when it is done.
+    worker.current.postMessage({ type: "interrupt" });
+  }
+
+  useEffect(() => {
+    resizeInput();
+  }, [input]);
+
+  function resizeInput() {
+    if (!textareaRef.current) return;
+
+    const target = textareaRef.current;
+    target.style.height = "auto";
+    const newHeight = Math.min(Math.max(target.scrollHeight, 24), 200);
+    target.style.height = `${newHeight}px`;
+  }
+
+  // We use the `useEffect` hook to setup the worker as soon as the `App` component is mounted.
+  useEffect(() => {
+    // Create the worker if it does not yet exist.
+    if (!worker.current) {
+      worker.current = new Worker(new URL("./worker.js", import.meta.url), {
+        type: "module",
+      });
+      worker.current.postMessage({ type: "check" }); // Do a feature check
+    }
+
+    // Create a callback function for messages from the worker thread.
+    const onMessageReceived = (e) => {
+      switch (e.data.status) {
+        case "loading":
+          // Model file start load: add a new progress item to the list.
+          setStatus("loading");
+          setLoadingMessage(e.data.data);
+          break;
+
+        case "initiate":
+          setProgressItems((prev) => [...prev, e.data]);
+          break;
+
+        case "progress":
+          // Model file progress: update one of the progress items.
+          setProgressItems((prev) =>
+            prev.map((item) => {
+              if (item.file === e.data.file) {
+                return { ...item, ...e.data };
+              }
+              return item;
+            }),
+          );
+          break;
+
+        case "done":
+          // Model file loaded: remove the progress item from the list.
+          setProgressItems((prev) =>
+            prev.filter((item) => item.file !== e.data.file),
+          );
+          break;
+
+        case "ready":
+          // Pipeline ready: the worker is ready to accept messages.
+          setStatus("ready");
+          break;
+
+        case "start":
+          {
+            // Start generation
+            setMessages((prev) => [
+              ...prev,
+              { role: "assistant", content: "" },
+            ]);
+          }
+          break;
+
+        case "update":
+          {
+            // Generation update: update the output text.
+            // Parse messages
+            const { output, tps, numTokens, state } = e.data;
+            setTps(tps);
+            setNumTokens(numTokens);
+            setMessages((prev) => {
+              const cloned = [...prev];
+              const last = cloned.at(-1);
+              const data = {
+                ...last,
+                content: last.content + output,
+              };
+              if (data.answerIndex === undefined && state === "answering") {
+                // When state changes to answering, we set the answerIndex
+                data.answerIndex = last.content.length;
+              }
+              cloned[cloned.length - 1] = data;
+              return cloned;
+            });
+          }
+          break;
+
+        case "complete":
+          // Generation complete: re-enable the "Generate" button
+          setIsRunning(false);
+          break;
+
+        case "error":
+          setError(e.data.data);
+          break;
+      }
+    };
+
+    const onErrorReceived = (e) => {
+      console.error("Worker error:", e);
+    };
+
+    // Attach the callback function as an event listener.
+    worker.current.addEventListener("message", onMessageReceived);
+    worker.current.addEventListener("error", onErrorReceived);
+
+    // Define a cleanup function for when the component is unmounted.
+    return () => {
+      worker.current.removeEventListener("message", onMessageReceived);
+      worker.current.removeEventListener("error", onErrorReceived);
+    };
+  }, []);
+
+  // Send the messages to the worker thread whenever the `messages` state changes.
+  useEffect(() => {
+    if (messages.filter((x) => x.role === "user").length === 0) {
+      // No user messages yet: do nothing.
+      return;
+    }
+    if (messages.at(-1).role === "assistant") {
+      // Do not update if the last message is from the assistant
+      return;
+    }
+    setTps(null);
+    worker.current.postMessage({ type: "generate", data: messages });
+  }, [messages, isRunning]);
+
+  useEffect(() => {
+    if (!chatContainerRef.current || !isRunning) return;
+    const element = chatContainerRef.current;
+    if (
+      element.scrollHeight - element.scrollTop - element.clientHeight <
+      STICKY_SCROLL_THRESHOLD
+    ) {
+      element.scrollTop = element.scrollHeight;
+    }
+  }, [messages, isRunning]);
+
+  return IS_WEBGPU_AVAILABLE ? (
+    <div className="px-10 mt-10">
+      
+        <div className="text-gray-400">
+          
+            
+            <p><b>SmolLM2-1.7B-Instruct WebGPU</b></p>
+        
+          
+          {status !== "ready" && (
+          <div>
+          
+            <p className="mb-4">
+            
+              <br />
+              You are about to load{" "}
+              <a
+                href="https://huggingface.co/HuggingFaceTB/SmolLM2-1.7B-Instruct"
+                target="_blank"
+                rel="noreferrer"
+                className="underline"
+              >
+                SmolLM2-1.7B-Instruct
+              </a>
+              , a 1.7B parameter reasoning LLM optimized for in-browser
+              inference. Everything runs entirely in your browser with{" "}
+              <a
+                href="https://huggingface.co/docs/transformers.js"
+                target="_blank"
+                rel="noreferrer"
+                className="underline"
+              >
+                transformers.js
+              </a>{" "}
+              and ONNX Runtime Web, meaning no data is sent to a server. Once
+              loaded, it can even be used offline.
+            </p>
+
+            {error && (
+              <div className="text-red-500 text-center mb-2">
+                <p className="mb-1">
+                  Unable to load model due to the following error:
+                </p>
+                <p className="text-sm">{error}</p>
+              </div>
+            )}
+            
+          </div>)}
+        </div>
+      
+      {status === null && (
+            <button
+              className="border border-gray-300 text-gray-500 rounded-lg p-2 hover:bg-gray-200 disabled:bg-blue-100 cursor-pointer disabled:cursor-not-allowed select-none mb-4 flex items-center gap-4"
+              onClick={() => {
+                worker.current.postMessage({ type: "load" });
+                setStatus("loading");
+              }}
+              disabled={status !== null || error !== null}
+            ><DownloadCloud size={14}/>
+              Load model
+            </button>)}
+            
+      {status === "loading" && (
+        <>
+          <div className="text-gray-300">
+            <p className="mt-4 mb-1">{loadingMessage}</p>
+            {progressItems.map(({ file, progress, total }, i) => (
+              <Progress
+                key={i}
+                text={file}
+                percentage={progress}
+                total={total}
+              />
+            ))}
+          </div>
+        </>
+      )}
+      
+      <br></br>
+      <hr className="border-gray-300"></hr>
+      <br></br>
+      
+      {status === "ready" && (
+        <div ref={chatContainerRef} className="overflow-y-auto scrollbar-thin w-full flex flex-col h-full">
+          <Chat messages={messages} />
+          <br></br><br></br><br></br><br></br><br></br><br></br>
+        </div>
+      )}
+
+      {status === "ready" && (
+        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2">
+          <div className="text-center">
+          <p className="min-h-6 text-gray-500 dark:text-gray-300 mt-4">
+            {tps && messages.length > 0 && (
+              <>
+                {!isRunning && (
+                  <span>
+                    Generated {numTokens} tokens in{" "}
+                    {(numTokens / tps).toFixed(2)} seconds&nbsp;&#40;
+                  </span>
+                )}
+                {
+                  <>
+                    <span className="text-center mr-1">
+                      {tps.toFixed(2)}
+                    </span>
+                    <span className="text-gray-500 dark:text-gray-300">
+                      tokens/second
+                    </span>
+                  </>
+                }
+                {!isRunning && (
+                  <>
+                    <span className="mr-1">&#41;.</span>
+                    <span
+                      className="underline cursor-pointer"
+                      onClick={() => {
+                        worker.current.postMessage({ type: "reset" });
+                        setMessages([]);
+                      }}
+                    >
+                      Reset
+                    </span>
+                  </>
+                )}
+              </>
+            )}
+          </p>
+          </div>
+
+          {isRunning && (
+          <div className="flex items-center justify-center mt-2">
+          <button onClick={onInterrupt}
+              className="border border-gray-300 text-gray-500 rounded-lg p-2 hover:bg-gray-200 disabled:bg-blue-100 cursor-pointer disabled:cursor-not-allowed select-none mb-4 flex items-center gap-4"
+              ><PauseCircle size={14}/>Stop</button>
+          </div>)}
+      
+      {!isRunning && (
+      <div className="mt-4 border border-gray-200 dark:bg-gray-700 mb-4 relative flex shadow-lg">
+        <textarea
+          ref={textareaRef}
+          className="scrollbar-thin w-[550px] dark:bg-gray-700 px-3 py-3 bg-transparent border-none outline-hidden text-gray-800 disabled:text-gray-400 dark:text-gray-200 placeholder-gray-500 dark:placeholder-gray-400 disabled:placeholder-gray-200 resize-none disabled:cursor-not-allowed"
+          placeholder="Type your message..."
+          type="text"
+          rows={1}
+          value={input}
+          disabled={status !== "ready"}
+          title={status === "ready" ? "Model is ready" : "Model not loaded yet"}
+          onKeyDown={(e) => {
+            if (
+              input.length > 0 &&
+              !isRunning &&
+              e.key === "Enter" &&
+              !e.shiftKey
+            ) {
+              e.preventDefault(); // Prevent default behavior of Enter key
+              onEnter(input);
+            }
+          }}
+          onInput={(e) => setInput(e.target.value)}
+        />
+        {/* {input.length > 0 ? ( */}
+          <div className="cursor-pointer" onClick={() => onEnter(input)}>
+            <ArrowRightIcon
+              className={`h-6 w-6 p-1 bg-gray-800 dark:bg-gray-100 text-white dark:text-black rounded-md absolute right-3 bottom-3`}
+            />
+          </div>
+        {/* ) : (
+          <div>
+            <ArrowRightIcon
+              className={`h-6 w-6 p-1 bg-gray-200 dark:bg-gray-600 text-gray-50 dark:text-gray-800 rounded-md absolute right-3 bottom-3`}
+            />
+          </div>
+        )} */}
+      </div>)}
+
+      </div> )}
+
+      <br></br>
+    </div>
+  ) : (
+    <div className="fixed w-screen h-screen bg-black z-10 bg-opacity-[92%] text-white text-2xl font-semibold flex justify-center items-center text-center">
+      WebGPU is not supported
+      <br />
+      by this browser :&#40;
+    </div>
+  );
+}
+
+export default App;
